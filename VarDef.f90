88e013a1663fabec21c75e2f32b35602457c2319
Alberto Paparella <alberto.paparella@edu.unife.it>
2021-06-23 12:06:39 +0200

    Fixed VarDef for 2D version

--- VarDef.f90
+++ VarDef.f90
@@ -4,23 +4,26 @@ MODULE VarDef_mod
     
     ! Geometry
     INTEGER             :: IMAX     ! total number of cells
-    INTEGER             :: JMAX     ! Max index for y coordinates
+    INTEGER             :: JMAX     ! 2D: Max index for y coordinates
     REAL                :: xL, xR   ! domain boundaries for axis x
-    REAL                :: yB,yU    ! domain boundaries for axis y
-    real                :: s        ! initial condition
+    REAL                :: yD,yU    ! 2D: domain boundaries for axis y
+    real                :: s        ! 2D: initial condition
     REAL, ALLOCATABLE   :: x(:)     ! vertex coords     (where u is defined)
+    REAL, ALLOCATABLE   :: y(:)     ! 2D: vertex y coords
     REAL, ALLOCATABLE   :: xb(:)    ! barycenter coords (where eta is defined)
+    REAL, ALLOCATABLE   :: yb(:)    ! 2D: barycenter y coords
     REAL                :: dx, dx2  ! mesh spacing
+    REAL                :: dy, dy2  ! 2D: mesh spacing on the y axis
     
     ! Discretization
     REAL                :: CFL      ! CFL number for time step
     REAL, PARAMETER     :: G=9.81   ! gravity acceleration
     
-    REAL, ALLOCATABLE   :: u(:)     ! velocity
-    REAL, ALLOCATABLE   :: Fu(:)    ! convective and viscous terms operator
-    REAL, ALLOCATABLE   :: eta(:)   ! pressure
-    REAL, ALLOCATABLE   :: H(:)     ! total wather depth
-    REAL, ALLOCATABLE   :: b(:)     ! bottom elevation
+    REAL, ALLOCATABLE   :: u(:, :)     ! velocity
+    REAL, ALLOCATABLE   :: Fu(:, :)    ! convective and viscous terms operator
+    REAL, ALLOCATABLE   :: eta(:, :)   ! pressure
+    REAL, ALLOCATABLE   :: H(:, :)     ! total wather depth
+    REAL, ALLOCATABLE   :: b(:, :)     ! bottom elevation
     !REAL, ALLOCATABLE   :: rhs(:)  ! rhs of the pressure system
     REAL                :: nu       ! kinematic viscosity coefficient
     
@@ -40,17 +43,19 @@ SUBROUTINE Allocate_Var
     IMPLICIT NONE
     
     ALLOCATE( x(IMAX+1), xb(IMAX+1) )
-    ALLOCATE( x(JMAX+1), xb(JMAX+1) )
+    ALLOCATE( y(JMAX+1), yb(JMAX+1) )   ! 2D
     x  = 0.
     xb = 0.
+    y  = 0. ! 2D
+    yb = 0. ! 2D
     
-    ALLOCATE( u(IMAX+1), Fu(IMAX+1) )
-    ALLOCATE( u(JMAX+1), Fu(JMAX+1) )
-    ALLOCATE( H(IMAX+1), b(IMAX+1)  )
-    ALLOCATE( H(JMAX+1), b(JMAX+1)  )
-    ALLOCATE( eta(IMAX)   )
-     ALLOCATE( eta(JMAX)   )
-    !ALLOCATE( eta(IMAX), rhs(IMAX)  )
+    ! ALLOCATE( u(IMAX+1), Fu(IMAX+1) )
+    ALLOCATE( u(IMAX+1, JMAX+1), Fu(IMAX+1, JMAX+1) )   ! 2D
+    ! ALLOCATE( H(IMAX+1), b(IMAX+1)  )
+    ALLOCATE( H(IMAX+1, JMAX+1), b(IMAX+1, JMAX+1)  )   ! 2D
+    ! ALLOCATE( eta(IMAX)             )
+    ALLOCATE( eta(IMAX, JMAX)                       )   ! 2D
+    !ALLOCATE( eta(IMAX), rhs(IMAX)   )
     u   = 0.
     Fu  = 0.
     H   = 0.
@@ -63,6 +68,7 @@ END SUBROUTINE Allocate_Var
 SUBROUTINE Deallocate_Var
     IMPLICIT NONE
     DEALLOCATE( x, xb    )
+    DEALLOCATE( y, yb    )
     DEALLOCATE( u, Fu    )
     DEALLOCATE( H, b     )
     DEALLOCATE( eta      )
