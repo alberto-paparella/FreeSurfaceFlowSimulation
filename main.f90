18fd702a0917e18d8c485dca402b099d06770fbe
Alberto Paparella <alberto.paparella@edu.unife.it>
2021-08-03 11:06:10 +0200

    FIxed updating velocity for both x and y.

--- main.f90
+++ main.f90
@@ -162,7 +162,7 @@ PROGRAM FS2D
             EXIT    
         ENDIF
         !==============================================================================================!
-        umax = MAXVAL( ABS(u) )             ! semi-implicit time step
+        umax = MAXVAL( ABS(u) )             ! Semi-implicit time step
         dt   = MIN( dt_fix, CFL / ( ( umax + 1e-14 ) / dx + 2. * nu / dx2 ) )
         dt2   = dt**2
         IF( ( time + dt ).GT.tend ) THEN
@@ -207,43 +207,7 @@ PROGRAM FS2D
                         + dt * ( nu/dy2 + (av-v(i,j))/(2.*dy) ) * v(i,j+1) &
                         + dt * ( nu/dy2 + (av+v(i,j))/(2.*dy) ) * v(i,j-1)
             ENDDO
-        ENDDO      
-        !==============================================================================================!
-        ! I'm pretty sure the following code (old code for 3.2 and 3.3, 20-07) is wrong
-        !==============================================================================================!
-        !
-        ! 3.2) Compute the operator Fu
-        !
-        ! BC: no-slip wall
-        !Fu(1,1)      = u(1,1)
-        !Fu(IMAX+1,JMAX) = u(IMAX+1,JMAX)  ! 2D    !qui mi crea una eccezione
-        !
-        !is not needed because our formula does not include viscosity,
-        !so we have to take the convention equation, with a being our velocity u,
-        !and add and subtract the other velocity, depending on whether we take information
-        !against redirection, propagation of information
-        !DO i = 2, IMAX
-            !DO j = 2, JMAX          ! 2D
-                ! au = ABS(u(i))
-                !au = ABS(u(i,j))    ! 2D
-                ! Explicit upwind
-                ! NB: the next line will always return eta(i,j), because it substract from it
-                !   a multiplication by 0
-                !Fu(i,j) = (eta(i,j)) - u(i+1,j)*(dt/dx * ( eta(i,j) - eta(i,j) ) )    ! 2D to try, if it doesn't work you have to find another formula with the upwind method
-            !ENDDO   ! 2D
-        !ENDDO                
-        !  
-        ! 3.3) Compute the operator Fv
-        !
-        ! BC: no-slip wall
-        !Fv(1, 1)           = v(1,1)
-        !Fv(IMAX,JMAX+1) = v(IMAX,JMAX+1)
-        !DO i = 2, IMAX
-            !DO j = 2, JMAX
-                ! Explicit upwind
-                !Fv(i,j) = (eta(i,j)) - v(i,j+1)*(dt/dy * ( eta(i,j) - eta(i,j) )  )   ! 2D to try, if it doesn't work you have to find another formula with the upwind method
-            !ENDDO   ! 2D
-        !ENDDO
+        ENDDO
         !==============================================================================================!
         ! 3.4) Solve the free surface equation
         !==============================================================================================!
@@ -258,75 +222,53 @@ PROGRAM FS2D
         ! Here, we suppose that IMAX and JMAX are the same, at least for the moment
         CALL CG(IMAX,eta,rhs)
         !==============================================================================================!
-      ! 3.4) Update the velocity (momentum equation)
-      !
-      ct = g*dt/dx  ! temporary coefficient
-      cs = g*dt/dy ! temporary coefficient
-      !
-      !u(1)      = Fu(1)
-      u(1,:)      = Fu(1,:) 
-      u(:,1)      = Fu(:,1)   ! 2D
-      !u(IMAX+1) = Fu(IMAX+1)
-      u(IMAX+1,:) = Fu(IMAX+1,:)  ! 2D
-      !DO i = 2, IMAX
-      !  u(i) = Fu(i) - ct * ( eta(i) - eta(i-1) )  
-      !ENDDO
-      DO i = 2, IMAX                                          ! 2D
-          DO j = 2, JMAX                                        ! 2D
-            u(i,j) = Fu(i,j) - ct * ( eta(i,j) - eta(i-1,j-1) ) ! 2D
-          ENDDO                                                 ! 2D
-      ENDDO                                                     ! 2D
-      !
-      time = time + dt  ! update time
-      !
-      
-        !
-      !u(1)      = Fu(1)
-      v(1,:)      = Fv(1,:) 
-      v(:,1)      = Fv(:,1)   ! 2D                ! 2D
-      !u(IMAX+1) = Fu(IMAX+1)
-      v(:,JMAX+1) = Fv(:,JMAX+1)  ! 2D
-      !DO i = 2, IMAX
-      !  u(i) = Fu(i) - ct * ( eta(i) - eta(i-1) )  
-      !ENDDO
-      DO i = 2, IMAX                                            ! 2D
-          DO j = 2, JMAX                                        ! 2D
-            v(i,j) = Fu(i,j) - cs * ( eta(i,j) - eta(i-1,j-1) ) ! 2D
-          ENDDO                                                 ! 2D
-      ENDDO 
-      ! 3.5) Eventually plot the results
-      IF(ABS(time-tio).LT.1e-12) THEN
-        WRITE(*,'(a,f15.7)') ' |   plotting data output at time ', time
-        CALL DataOutput(n)
-        tio = tio + dtio
-      ENDIF  
-      !
-    ENDDO !n
-    !
+        ! 3.5) Update the velocity (momentum equation)
+        !==============================================================================================!
+        ct = g*dt/dx    ! Temporary coefficient for x
+        cs = g*dt/dy    ! Temporary coefficient for y
+        !==============================================================================================!
+        u(1,:)      = Fu(1,:) 
+        u(IMAX+1,:) = Fu(IMAX+1,:)
+        DO i = 2, IMAX
+            DO j = 1, JMAX
+                u(i,j) = Fu(i,j) - ct * ( eta(i,j) - eta(i-1,j) )
+            ENDDO
+        ENDDO
+        !==============================================================================================!
+        v(:,1)      = Fv(:,1)
+        v(:,JMAX+1) = Fv(:,JMAX+1)
+        DO i = 1, IMAX
+            DO j = 2, JMAX
+                v(i,j) = Fu(i,j) - cs * ( eta(i,j) - eta(i,j-1) )
+            ENDDO
+        ENDDO 
+        !==============================================================================================!
+        time = time + dt  ! Update time
+        !==============================================================================================!
+        ! 3.6) Eventually plot the results
+        IF(ABS(time-tio).LT.1e-12) THEN
+            WRITE(*,'(a,f15.7)') ' |   plotting data output at time ', time
+            CALL DataOutput(n)
+            tio = tio + dtio
+        ENDIF  
+    !==============================================================================================!
+    ENDDO ! n cycle
+    !==============================================================================================!
     WRITE(*,'(a)') ' | END of the COMPUTATION '
-    !
-    !---------------------------------------- 
-    !---------------------------------------- 
-    !
+    !==============================================================================================!
     ! Empty memory
-    !
+    !==============================================================================================!
     CALL Deallocate_Var
-    !
+    !==============================================================================================!
     DEALLOCATE( amat,bmat,cmat, amatj,cmatj )
-    !
+    !==============================================================================================!
     WRITE(*,'(a)') ' | '
     WRITE(*,'(a)') ' |         Finalization was successful. Bye :-)           | '
     WRITE(*,'(a)') ' | ====================================================== | '
-    !
-    
+    !==============================================================================================!    
     PAUSE
-END PROGRAM FS2D 
+END PROGRAM FS2D
 
-!======================================================================================================!
-! matop2D should calculate matrix-matrix product.
-! However, at the moment it is a changed version of matop1D considering 2 dimensions,
-! and there are some problems with cases (i = 1 and j != 1, i != 1 and j = 1, and expecially
-! i = 1 and j = IMAX, and i = IMAX and j = 1.
 !======================================================================================================!
 SUBROUTINE matop2D(Ap,p,N)
     !==================================================================================================!
@@ -414,67 +356,3 @@ SUBROUTINE matop2D(Ap,p,N)
         !
     ENDDO
 END SUBROUTINE matop2D
-
-!======================================================================================================!
-! Old version of matop2D (20-07)
-!======================================================================================================!
-SUBROUTINE OLDmatop2D(Ap,p,N)  
-    !------------------------------------------------------------!
-    USE VarDef_mod
-    IMPLICIT NONE
-    !------------------------------------------------------------!
-    INTEGER  :: N 
-    REAL     :: Ap(N,N), p(N,N)
-    !
-    INTEGER  :: i, j
-    REAL     :: ct, cs , amat, bmat, cmat
-    REAL     :: amatj , cmatj
-    !------------------------------------------------------------!
-    !
-    ct = g*dt2/dx2  ! temporary coefficient
-    cs = g*dt2/dy2
-    DO i = 2, N
-      DO j = 2, N   ! 2D
-          if(i.eq.250) then
-            continue
-          endif  
-          IF(i.EQ.1) THEN
-             IF(j.EQ.1) THEN
-                !bmat    = 1. + ct * ( H(i+1) + H(i) )
-                bmat    = 1. + ct * ( Hu(i+1,j) + 0.0) + cs*( Hv(i,j+1) + 0.0 )
-                !cmat    = - ct * H(i+1)
-                cmat    = - ct * Hu(i+1,j)                    ! 2D
-                cmatj   = - cs * Hv(i,j+1)                    ! 2D
-            
-                Ap(i,j) = bmat*p(i,j) + cmat*p(i,j)  + cmatj*p(i,j)        ! 2D
-            ELSEIF(i.EQ.N) THEN  
-                ELSEIF(j.EQ.N) THEN 
-                    !amat    = - ct * H(i)
-                    amat    = - ct * Hu(i,j-1)                        ! 2D
-                    amatj   = - cs * Hv(i-1,j)                         ! 2D
-                    !bmat    = 1. + ct * ( 0.0 + Hu(i,j) )   ! 2D    
-                    bmat    = 1. + ct * ( 0.0 + Hu(i,j)) + cs*( 0.0 + Hv(i,j) )  ! 2D
-                    
-                    Ap(i,j) = amat*p(i-1,j) + amatj*p(i,j-1) + bmat*p(i,j)           ! 2D
-            ELSE 
-                !amat    = - ct * H(i)
-                amat    = - ct * Hu(i,j)                        ! 2D
-                amatj   = - cs * Hv(i,j)                        ! 2D
-                !bmat    = 1. + ct * ( H(i+1) + H(i) )
-                bmat    = 1. + ct * ( Hu(i+1,j) + Hu(i,j)) + cs*( Hv(i,j+1) + Hv(i,j) )  ! 2D
-                !cmat    = - ct * H(i+1)
-                cmat    = - ct * Hu(i+1,j)                    ! 2D
-                cmatj   = - cs * Hv(i,j+1)                    ! 2D
-                Ap(i,j) = amat*p(i-1,j) + amatj*p(i,j-1) + bmat*p(i,j) + cmat*p(i+1,j)  + cmat*p(i,j+1)         ! 2D
-            
-            ENDIF  
-          ENDIF
-          
-          !
-      ENDDO       ! 2D
-    ENDDO
-    
-  
-    
-    !
-    END SUBROUTINE OLDmatop2D
